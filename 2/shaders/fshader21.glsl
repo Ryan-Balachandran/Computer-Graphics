31b6f4a8b66826c39345358e961ef3de5b1071a9
Ryan-Balachandran <ryanbalachandran@yahoo.com>
2022-10-08 14:39:53 -0400

    Delete 2 directory

--- 2/shaders/fshader21.glsl
+++
@@ -1,9 +0,0 @@
-precision mediump float;
-uniform float fr;
-uniform float fg;
-uniform float fb;
-
-void main()
-{
-    gl_FragColor = vec4(fr, fg, fb, 1.0);
-}
--- 2/shaders/vshader21.glsl
+++
@@ -1,8 +0,0 @@
-attribute vec4 vPosition;
-uniform float vPointSize;
-
-void main() 
-{
-    gl_PointSize = vPointSize;
-    gl_Position = vPosition;
-}
