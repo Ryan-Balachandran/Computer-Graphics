c274b34fa657bf9b804ad972694b9599078c9cde
Ryan-Balachandran <ryanbalachandran@yahoo.com>
2022-10-26 16:30:45 -0400

    Delete Scripts directory

--- Scripts/common.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-const s = {};		// use this singleton instead of global variables
-
-// use this as a shortcut for getting DOM objects
-s.g = function (id) { return document.getElementById(id); }
-s.q = function (id) { return document.querySelector(id); }
-s.qs = function (id) { return document.querySelectorAll(id); }
